140106712dbcd57c00e6821d8c133166a3dcbfb2
aparedes1204 <aitorpz1204@gmail.com>
2021-12-01 19:05:56 +0100

    PDO blob error fixed

--- php/SignUp.php
+++ php/SignUp.php
@@ -104,7 +104,7 @@
             $stmt->bindParam(2, $eposta);
             $stmt->bindParam(3, $deitura);
             $stmt->bindParam(4, $pasahitza);
-            $stmt->bindParam(5, $blob,PDO::PARAM_LOB);
+            $stmt->bindParam(5, $blob,PDO::PARAM_BLOB);
             $stmt->execute();
             //$sartu = mysqli_query($esteka, "INSERT INTO Users(erMota, eposta, deitura, pasahitza, argazkia) VALUES ('$erMota', '$eposta', '$deitura', '$pasahitza', '{$blob}')");
 
