634db9fbf0bd314c6ae7d71c097cc6e36d263fc2
aparedes1204 <aitorpz1204@gmail.com>
2021-12-19 15:39:33 +0100

    fb loging testing

--- js/FbLogin.js
+++ js/FbLogin.js
@@ -13,7 +13,40 @@ $(document).ready(function(){
     })
 })
 
-function testAPI() {                      
+function statusChangeCallback(response) {  
+    console.log('statusChangeCallback');
+    console.log(response);                 
+    if (response.status === 'connected') { 
+      testAPI();  
+    } else {                               
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
+  }
+
+
+  function checkLoginState() {             
+    FB.getLoginStatus(function(response) { 
+      statusChangeCallback(response);
+    });
+  }
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '943970049544335',
+      cookie     : true,                   
+      xfbml      : true,                   
+      version    : 'v12.0'           
+    });
+
+
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);  
+    });
+  };
+ 
+  function testAPI() {                    
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
